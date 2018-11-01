76b9c3d8cd8d99ebad345e3a6713e9af85b60e70
Sergey <pomazkin.sergey@gmail.com>
2018-11-01 22:21:03 +0500

    Добавлен код по уроку 4 ООП

---
+++ SNAKE/SNAKE/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNAKE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(3, 6, '*');
+            Point p2 = new Point(3, 10, '#');
+            p1.Draw();
+            p2.Draw();
+            Console.ReadLine();
+
+
+        }
+    }
+}
